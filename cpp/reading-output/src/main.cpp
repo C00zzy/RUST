91a29859df3c3c28c4b3668b8362f2d98c09231c
c0zzy <losergfg4@outlook.com>
2024-05-10 14:44:47 -0400

    new file:   cpp/reading-output/src/main 	new file:   cpp/reading-output/src/main.cpp 	new file:   cpp/temperature/src/main  new file:   cpp/temperature/src/main.cpp

---
+++ cpp/reading-output/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Enter an value: ";
+     double x;
+        double y;
+            cin >> x;
+                cin >> y; 
+                    cout << x + y;
+
+    
+    return 0;
+}
---
+++ cpp/temperature/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x;
+    int y;
+    int fuckyou = y;
+    int retard = x;
+    cout << "Which input? x or y?";
+    cin >> x;
+    cin >> y; 
+    
+    return 0;
+}
