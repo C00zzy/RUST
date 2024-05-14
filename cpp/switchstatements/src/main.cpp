9fcc4603897263b41a88c66a1d40f0df56f59583
Or <152229944+C00zzy@users.noreply.github.com>
2024-05-13 21:14:37 -0400

    learning i still suck

---
+++ cpp/switchstatements/src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+int month;
+cout << "Enter the month (1-12):  ";
+cin >> month;
+
+
+switch(month){
+    case 1:
+        cout >> "It is january";
+        break;
+          case 2:
+        cout >> "It is feburary";
+        break;
+          case 3:
+        cout >> "It is march";
+        break;
+          case 4:
+        cout >> "It is april";
+        break;
+          case 5:
+        cout >> "It is may";
+        break;
+}
