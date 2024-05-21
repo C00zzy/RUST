03cbbc053e0d32eeda78bbc492b3f3e7ead67bbc
C0zzy <losergfg4@outlook.com>
2024-05-21 15:15:40 -0400

    modified:   cpp/calculator/src/main.cpp

--- cpp/calculator/src/main.cpp
+++ cpp/calculator/src/main.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 
-using namespace std;
+
 
 
 int main(){
    int num1, num2;
     char opr;
-    cout << "Two numbers";
-    cin >> num1 >> num2;
-    cout << "Enter operator: ";
-    cin >> opr;
-    cout << endl;
+    std::cout << "Two numbers";
+    std::cin >> num1 >> num2;
+    std::cout << "Enter operator: ";
+    std::cin >> opr;
+    std::cout << std::endl;
      switch (opr){
         case '+':
-            cout << num1 + num2 << endl;
+            std::cout << num1 + num2 << std::endl;
         break;
         case '*':
-            cout << num1 * num2 << endl;
+            std::cout << num1 * num2 << std::endl;
             break;
             default:
-            cout << "you cant calculate others yet.." << endl;
+            std::cout << "you cant calculate others yet.." << std::endl;
     }
     return 0;
  
