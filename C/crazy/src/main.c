678bebb47a07a9d7d0bd07ee644d435f722eadb5
C0zzy <losergfg4@outlook.com>
2024-06-03 22:30:05 -0400

    new file:   C/crazy/src/main 	new file:   C/crazy/src/main.c 	modified:   C/malloc/src/main.c

---
+++ C/crazy/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+     char input[100];
+    printf("Enter your input\n");
+    scanf ("%s", &input);
+    printf("%s is crazy\n", input);
+}
