c319187887a926c63f3c150f374905867ecdf3ee
C0zzy <losergfg4@outlook.com>
2024-06-09 19:13:46 -0400

    new file:   python/algorithm1/src/main.py 	new file:   python/algorithm2/src/main.py

---
+++ python/algorithm1/src/main.py
@@ -0,0 +1,9 @@
+print("Starting")
+
+def logic():
+    num1 = 2
+    num2 = 5
+    sum = num1 + num2 
+    print(sum)
+logic()
+print("stopping")
---
+++ python/algorithm2/src/main.py
@@ -0,0 +1,19 @@
+print("starting")
+a = 2
+b = 3
+c = 4
+print(a, b, c)
+
+def read():
+    if a > c:
+        print("a is the biggest")
+    else:
+        print("C is the biggest")
+    if b > c:
+        print("b is the biggest")
+    else:
+        print("c is the biggest")
+        pass
+    print("stopping")
+read()
+        
