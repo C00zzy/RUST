991aa880f729f7e89b6dfca553291ea538820a28
C0zzy <losergfg4@outlook.com>
2024-06-08 21:21:17 -0400

    modified:   python/calculator/src/main.py 	new file:   python/tkinter/src/main.py

--- python/calculator/src/main.py
+++ python/calculator/src/main.py
@@ -1,20 +1,30 @@
-opr = input("Enter an operator")
-num1 = float(input("Enter an number"))
-num2  = float(input("Enter another number"))
-if opr == '+':
-    result = num1 + num2
-    print("Result:", result)
-elif opr == '*':
-    result = num1 * num2
-    print("Result:", result)
-elif opr == '/':
-    if num2 == 0:
-        print("Error: Division by zero")
-    else:
-        result = num1 / num2
-        print("Result:", result)
-elif opr == '-':
-    result = num1 - num2
-    print("Result:", result)
-else:
-    print("That's not a operator")
+def logicforcal():
+    while True:
+        try:
+            opr = input("Enter an operator")
+            if opr.lower() == 'q':
+                print("Exiting")
+                break
+            num1 = float(input("Enter an number"))
+            num2 = float(input("Enter an number"))
+            
+            if opr == '+':
+                result = num1 + num2
+                print("Result:", result)
+            elif opr == '*':
+                result = num1 + num2
+                print("Result:", result)
+            elif opr == '/':
+                if num2 == 0:
+                    print("Cannot divide by 0")
+                else:
+                    result / num1 + num2
+                    print("Result:", result)
+            elif opr == '-':
+                result - num1 + num2
+                print("Result:", result)
+            else:
+                print("Not valid")
+        except ValueError:
+            print("Not a valid number")
+logicforcal()
