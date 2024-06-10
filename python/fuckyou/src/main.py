188d55ada7b02a750a2cd3d0091ac78d9a147952
C0zzy <losergfg4@outlook.com>
2024-06-09 23:32:33 -0400

    modified:   python/fuckyou/src/main.py

--- python/fuckyou/src/main.py
+++ python/fuckyou/src/main.py
@@ -2,7 +2,7 @@ import random
 def numbers():
     while True:
         try:
-            print(random.randrange(3, 20))
+            print(random.randrange(3, 100000))
         except KeyboardInterrupt:
             print("stopped")
             break
