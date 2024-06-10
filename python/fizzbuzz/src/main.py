3225f367971a9eda2672a3614b63476030378772
C0zzy <losergfg4@outlook.com>
2024-06-10 19:48:13 -0400

    modified:   python/fizzbuzz/src/main.py

--- python/fizzbuzz/src/main.py
+++ python/fizzbuzz/src/main.py
@@ -8,6 +8,10 @@ while (num <= 100):
     
     elif (num % 3 == 0):
         print(str(num) + ": Fizz!")
+    elif (num % 5 == 0):
+        print(str(num) + ": Fizz!")
+    else:
+        print(str(num) + '.')
     
     print(num)
     num +=1
