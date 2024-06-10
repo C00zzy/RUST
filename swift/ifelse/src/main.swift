2c0a5541ab156c06cbe74cb109cc1a17fb3a893e
C0zzy <losergfg4@outlook.com>
2024-06-10 14:43:11 -0400

    new file:   swift/ifelse/src/main.swift 	new file:   swift/switches/src/main.swift

---
+++ swift/ifelse/src/main.swift
@@ -0,0 +1,11 @@
+let number = 40
+
+if number < 0{
+    print("The number is bad")
+}
+else if number == 0{
+    print("number 0")
+}
+else{
+    print("the number is positive")
+}
---
+++ swift/switches/src/main.swift
@@ -0,0 +1,10 @@
+let day: String = "Monday"
+
+switch day{
+    case "Monday":
+    print("it is monday")
+    case "tuesday":
+    print("it is tuesday")
+    default:
+    print("its the weekend")
+}
