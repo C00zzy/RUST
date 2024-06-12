029cadf4da1a4ec987624412cf70ec784280f070
C0zzy <losergfg4@outlook.com>
2024-06-11 21:00:06 -0400

    new file:   python/firstleetcode/src/first.py 	modified:   python/main.py

--- python/firstleetcode/src/first.py
+++ python/firstleetcode/src/first.py
@@ -1,5 +1,6 @@
 class Solution:
     def sortArray(self, nums: list[int]) -> list[int]:
         nums.sort()
-        nums.sort(reverse=True)
         return nums
+
+#this is my first leetcode question i completed on my own i only used examples
