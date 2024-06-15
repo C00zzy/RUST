7325a24516adf088f88e28f716e1305b3a166642
C0zzy <losergfg4@outlook.com>
2024-06-14 22:02:27 -0400

    deleted:    python/CalorieCounter/src/calories.txt 	deleted:    python/CalorieCounter/src/main.py 	deleted:    shell/Cargo.lock 	deleted:    shell/Cargo.toml 	deleted:    shell/src/main.rs 	deleted:    shell/target/.rustc_info.json 	deleted:    shell/target/CACHEDIR.TAG 	deleted:    shell/target/debug/.cargo-lock 	deleted:    shell/target/debug/.fingerprint/shell-3747b9c4faed83f1/bin-shell 	deleted:    shell/target/debug/.fingerprint/shell-3747b9c4faed83f1/bin-shell.json 	deleted:    shell/target/debug/.fingerprint/shell-3747b9c4faed83f1/dep-bin-shell 	deleted:    shell/target/debug/.fingerprint/shell-3747b9c4faed83f1/invoked.timestamp 	deleted:    shell/target/debug/.fingerprint/shell-e7293e92b182ff7e/dep-test-bin-shell 	deleted:    shell/target/debug/.fingerprint/shell-e7293e92b182ff7e/invoked.timestamp 	deleted:    shell/target/debug/.fingerprint/shell-e7293e92b182ff7e/test-bin-shell 	deleted:    shell/target/debug/.fingerprint/shell-e7293e92b182ff7e/test-bin-shell.json 	deleted:    shell/target/debug/deps/libshell-3747b9c4faed83f1.rmeta 	deleted:    shell/target/debug/deps/libshell-e7293e92b182ff7e.rmeta 	deleted:    shell/target/debug/deps/shell-3747b9c4faed83f1.d 	deleted:    shell/target/debug/deps/shell-e7293e92b182ff7e.d 	deleted:    shell/target/debug/incremental/shell-2zdolex8kkw27/s-gw1ksn9yck-1xujgur-dwmfxw8zc9hehg3d5k4azje5o/dep-graph.bin 	deleted:    shell/target/debug/incremental/shell-2zdolex8kkw27/s-gw1ksn9yck-1xujgur-dwmfxw8zc9hehg3d5k4azje5o/query-cache.bin 	deleted:    shell/target/debug/incremental/shell-2zdolex8kkw27/s-gw1ksn9yck-1xujgur-dwmfxw8zc9hehg3d5k4azje5o/work-products.bin 	deleted:    shell/target/debug/incremental/shell-2zdolex8kkw27/s-gw1ksn9yck-1xujgur.lock 	deleted:    shell/target/debug/incremental/shell-31wnswpozy77e/s-gw1ksn9yb8-1ppm9ix-3f2vxleu4it23kwvuovlx5jjv/dep-graph.bin 	deleted:    shell/target/debug/incremental/shell-31wnswpozy77e/s-gw1ksn9yb8-1ppm9ix-3f2vxleu4it23kwvuovlx5jjv/query-cache.bin 	deleted:    shell/target/debug/incremental/shell-31wnswpozy77e/s-gw1ksn9yb8-1ppm9ix-3f2vxleu4it23kwvuovlx5jjv/work-products.bin 	deleted:    shell/target/debug/incremental/shell-31wnswpozy77e/s-gw1ksn9yb8-1ppm9ix.lock

--- shell/target/debug/deps/shell-3747b9c4faed83f1.d
+++
@@ -1,5 +0,0 @@
-/home/nolan/git/RUST/shell/target/debug/deps/libshell-3747b9c4faed83f1.rmeta: src/main.rs
-
-/home/nolan/git/RUST/shell/target/debug/deps/shell-3747b9c4faed83f1.d: src/main.rs
-
-src/main.rs:
