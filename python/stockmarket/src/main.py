1b81b3eeb8a17b6a71f8d7bc316565198bc1fd8e
C0zzy <losergfg4@outlook.com>
2024-06-18 19:08:44 -0400

    modified:   python/stockmarket/src/main.py

--- python/stockmarket/src/main.py
+++ python/stockmarket/src/main.py
@@ -1,17 +1,17 @@
 import os
-
-def screenclear():
+# Stock market script concept
+def screenclear(): # This is a command line program
     if os.name == 'posix':
         os.system('clear')
     elif os.name == 'nt':
         os.system('cls')
 
 
-def checkcandleinput():
+def checkcandleinput(): # for input funcution
     while True:
         candle = input("Green for 1, Red for 0: ")
-        if candle in ["0", "1"]:
-            return candle
+        if candle in ["0", "1"]: # Checks if the input is 1 or 0 and if so candle is assigned to that
+            return candle # Returns to outside of funcution
         else:
             print("Invalid")
 
@@ -19,9 +19,8 @@ def checkcandleinput():
 def check_candle():
     candle = checkcandleinput()
     if candle == "1":
-        print("INVEST")
+        print("INVEST") # Main Logic
     elif candle == "0":
         print("STOP")
 screenclear()
 check_candle()
-
