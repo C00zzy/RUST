7a5c42915bc9c47b12ad3f95766a11d7c563421d
C0zzy <losergfg4@outlook.com>
2024-06-18 18:53:30 -0400

    modified:   .vscode/launch.json 	new file:   python/stockmarket/src/main.py

---
+++ python/stockmarket/src/main.py
@@ -0,0 +1,27 @@
+import os
+
+def screenclear():
+    if os.name == 'posix':
+        os.system('clear')
+    elif os.name == 'nt':
+        os.system('cls')
+
+
+def candleinput():
+    while True:
+        candle = input("Green for 1, Red for 0: ")
+        if candle in ["0", "1"]:
+            return candle
+        else:
+            print("Invalid")
+
+
+def check_candle():
+    candle = candleinput()
+    if candle == "1":
+        print("INVEST")
+    elif candle == "0":
+        print("STOP")
+screenclear()
+check_candle()
+
