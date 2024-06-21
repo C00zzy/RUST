59ea70acf648c8f883551a8758836882e2dfd845
C0zzy <losergfg4@outlook.com>
2024-06-21 12:46:19 -0400

    modified:   python/factorial/src/main.py

--- python/factorial/src/main.py
+++ python/factorial/src/main.py
@@ -1,6 +1,10 @@
+def userinput():
+    n = int(input("What number:  "))
+    solution = factorinal(n)
+    print(f"the factoral of {n} is: {solution}")
 def factorinal(n):
     if n == 0:
         return 1
     else:
         return n * factorinal(n - 1)
-print(factorinal(5))
+userinput()
