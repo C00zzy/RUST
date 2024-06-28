6c3e28a6f5f657689869947cd1872b38103144ca
C0zzy <losergfg4@outlook.com>
2024-06-28 18:55:13 -0400

    new file:   python/passwordgen/src/main.py

---
+++ python/passwordgen/src/main.py
@@ -0,0 +1,15 @@
+# Modules
+import random
+import string
+from typing import Literal
+
+# Funcution
+
+def randomletterandnums() -> None:
+    password: Literal[''] = ""
+    for _ in range(10):
+        randomnum: int = random.randrange(start=1, stop=30)
+        randomlet: str = random.choice(seq=string.ascii_letters)
+        password += str(object=randomnum) + randomlet
+    print(password)
+randomletterandnums()
