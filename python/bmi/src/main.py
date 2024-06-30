1022296bcc66b33ccccdf4f1635bbcfc8d6b9acc
C0zzy <losergfg4@outlook.com>
2024-06-29 22:03:35 -0400

    new file:   python/bmi/src/main.py 	modified:   python/passwordgen/src/main.py

---
+++ python/bmi/src/main.py
@@ -0,0 +1,5 @@
+weight = float(input("What is your weight"))
+height = float(input("What is your height"))
+
+bmi = weight / (height ** 2)
+print(bmi)
--- python/passwordgen/src/main.py
+++ python/passwordgen/src/main.py
@@ -5,8 +5,9 @@ import typing
 # funcution
 
 def randomletterandnums() -> None:
+    n = int(input("How long do you want your password: "))
     password: Literal[''] = ""
-    for _ in range(10): # does this until it hits the quota
+    for _ in range(n): # does this until it hits the quota
         randomnum: int = random.randrange(start=1, stop=10) # Generates numbers
         randomlet: str = random.choice(seq=string.ascii_letters) # Generates lettersj
         password += str(object=randomnum) + randomlet # Adds them together
