25e1364ae5174c57221205e9477e1cf62d45bedb
C00zzy <losergfg4@outlook.com>
2024-07-05 18:28:22 -0400

    new file:   C/a.out 	modified:   C/main.c

--- C/main.c
+++ C/main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
-// Variables
-int main ()
-{
-int num;
-printf("Enter number");
-scanf("%d", &num);
-printf("You enter %d\n", &num);
+int main(void){
+    int i;
+    for(int i = 0; i <= 100; i++) // i is 0 if i is less than 100 it will add to i
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+            printf("FizzBuzz\n");
+        else if (i % 3 == 0){
+            printf("Buzz\n");
+        }
+        else if (i % 5 == 0){
+            printf("Fizz\n");
+        } 
+        else{
+            printf("%d\n", i);
+        }
+    }
 }
