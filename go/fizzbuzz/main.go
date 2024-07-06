6ac0c27fd59fa339f5b25da1ddd4e12234f20478
C00zzy <losergfg4@outlook.com>
2024-07-06 17:11:50 -0400

    modified:   go/fizzbuzz/main.go

--- go/fizzbuzz/main.go
+++ go/fizzbuzz/main.go
@@ -2,17 +2,26 @@ package main
 
 import "fmt"
 
-func main() {
-	for i := 0; i < 100; i++ {
-		switch {
-		case i%5 == 0 && i&3 == 0:
-			fmt.Println("FizzBuzz")
-		case i%3 == 0:
-			fmt.Println("Fizz")
-		case i%5 == 0:
-			fmt.Println("Buzz")
-		default:
-			fmt.Println(i)
-		}
+// fizzbuzz
+
+func fizzbuzz(value int) string {
+	if value%3 == 0 && value%6 == 0 {
+		return "FizzBuzz"
+	} else if value%3 == 0 {
+		return "Fizz"
+	} else if value%5 == 0 {
+		return "Buzz"
+	} else {
+		return fmt.Sprint("NONE: ", value)
 	}
+}
+
+func forloop(start, end int, action func(int) string) {
+	for i := start; i <= end; i++ {
+		fmt.Println(action(i))
+	}
+}
+
+func main() {
+	forloop(1, 100, fizzbuzz)
 }
