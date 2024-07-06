0227ad6ea5f4c996157fe884950595798fd4d06a
C00zzy <losergfg4@outlook.com>
2024-07-06 14:17:52 -0400

    new file:   python/main2.py

---
+++ python/main2.py
@@ -0,0 +1,4 @@
+def hello_world():
+    return ("Hello, World")
+# Funcutional programming because it doesn't modify anything thus meaning no side affects and it treats funcutions as first class
+print(hello_world())
