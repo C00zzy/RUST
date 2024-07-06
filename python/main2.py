032dfffce2aa0ac2ce8f551d4f0f996134db1121
C00zzy <losergfg4@outlook.com>
2024-07-06 19:16:49 -0400

    modified:   python/main2.py

--- python/main2.py
+++ python/main2.py
@@ -1,11 +1,18 @@
-import random
-def returnvalue(value):
-    return value
-def printforvalue(value):
-    print(value)
+# Exercise: Filter and Map
+#
+# Write a function that takes a list of numbers and does the following:
+#
+# 1. Filters out all even numbers.
+# 2. Doubles each remaining number.
+# 3. Returns the modified list.
+#
+# For example, given the list [1, 2, 3, 4, 5], the function should return [2, 6, 10].
+def filter0(begin, end):
+    numbers = []
+    for i in range(begin,end):
+        if i % 2 != 0:
+            numbers.append(i * 2)
+    return numbers
+print(filter0(1, 6))
 
-def forloop(start, end, func,):
-    for i in range(start, end + 1):
-        func(i)
-
-forloop(1, 100, printforvalue)
+# I was close for this problem.
