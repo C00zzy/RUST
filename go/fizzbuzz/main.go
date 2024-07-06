862083641fecbea9525c7652884af617413655be
C00zzy <losergfg4@outlook.com>
2024-07-06 12:17:02 -0400

    modified:   go/fizzbuzz/main 	modified:   go/fizzbuzz/main.go

--- go/fizzbuzz/main.go
+++ go/fizzbuzz/main.go
@@ -4,13 +4,14 @@ import "fmt"
 
 func main() {
 	for i := 0; i < 100; i++ {
-		fmt.Println(i)
 		if i%3 == 0 && i%5 == 0 {
 			fmt.Println("FizzBuzz")
 		} else if i%3 == 0 {
 			fmt.Println("Buzz")
 		} else if i%5 == 0 {
 			fmt.Println("Fizz")
+		} else {
+			fmt.Println(i)
 		}
 	}
 }
