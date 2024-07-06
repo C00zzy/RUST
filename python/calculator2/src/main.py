dde0bc613e8a664985711678cb9373a45fb1343b
C00zzy <losergfg4@outlook.com>
2024-07-06 14:08:32 -0400

    modified:   python/calculator2/src/main.py 	modified:   python/main.py

--- python/calculator2/src/main.py
+++ python/calculator2/src/main.py
@@ -1,61 +1,55 @@
-import os 
 
-def screenclear():
-    if os.name == 'posix':
-        os.system('clear')
-    elif os.name == 'nt':
-        os.system('cls')
+# First Funcution
 
-def get_numbers():
+def getnum():
     while True:
         try:
-            num1, num2, = input("Enter two number seperated by a comma:  ").split(',')
-            
+            num1, num2 = input("Two numbers").split(',')
             num1 = float(num1.strip())
-            num2 = float(num2.strip()) 
-            
+            num2 = float(num2.strip())
             return num1, num2
         except KeyboardInterrupt:
-            print("Exiting")
+            print("Leaving")
             break
         except ValueError:
             print("Invalid")
-
+# Second Funcution
 def addition(num1, num2):
     return num1 + num2
-
-def multiplication(num1, num2):
-    return num1 * num2
-
-def divde(num1, num2):
-    if num1 != 0:
+# Third Funcution
+def substraction(num1, num2):
+    return num1 - num2
+# Fourth Funcution
+def dividison(num1, num2):
+    if num2 != 0:
         return num1 / num2
     else:
         return("ERROR")
-def substract(num1, num2):
-    return num1 - num2
-
+# Fifth Funcution
+def multi(num1 ,num2):
+    return num1 * num2
+def calculate(opr, num1, num2):
+    if opr == '+':
+        return addition(num1, num2)
+    if opr == '*':
+        return multi(num1, num2)
+    if opr == '/':
+        return dividison(num1, num2)
+    if opr == '-':
+        return substraction(num1, num2)
+    else:
+        return ("Invalid")
+def calculator():
+    opr = get_opr()
+    num1, num2 = getnum()
+    solution = calculate(opr, num1, num2)
+    print(solution)
 def get_opr():
     while True:
-        opr = input("Enter operator: ")
-        if opr in ['+', '*', '/', '-']:
-            return opr
-        else:
-            print("invalid")
-def calculator():
-    screenclear()
-    operator = get_opr()
-    num1, num2 = get_numbers()
-    if operator == '+':
-        solution = addition(num1, num2)
-        print(f"sum is: {solution}")
-    elif operator == '*':
-        solution = multiplication(num1, num2)
-        print(f"sum is: {solution}")
-    elif operator == '/':
-        solution = divde(num1, num2)
-        print(f"sum is: {solution}")
-    elif operator == '-':
-        solution = substract(num1, num2)
-        print(f"sum is: {solution}")
+            opr = input("Enter opr")
+            if opr in ['+', '*', '/', '-']:
+                return opr
+            else:
+                print("Error")
 calculator()
+#redone with funcutional programming
--- python/main.py
+++ python/main.py
@@ -1,2 +1,48 @@
-for i in range(1,100000):
-    print(i)
+import keyboard
+import time
+
+def press_key(key):
+    keyboard.press(key)
+    time.sleep(0.1)
+    keyboard.release(key)
+
+def keypress_a():
+    press_key('a')
+
+def keypress_b():
+    press_key('b')
+
+def key_loop(key_funcution):
+    try:
+        while True:
+            key_funcution()
+    except KeyboardInterrupt:
+        print("Exiting")
+key_loop(keypress_a)
+
+"""
+# Original version (not functional programming)
+def keyboardpress():
+    keyboard.press(a)
+    time.sleep(10)
+    keyboard.press(b)
+    keyboard.release(a)
+    keyboard.release(b)
+
+# Reasons:
+# A. It doesn't treat functions as first-class citizens.
+# B. It has side effects instead of being a function with no side effects.
+"""
+
+"""
+# Refactored version (following functional programming)
+def press_key(key):
+    keyboard.press(key)
+    time.sleep(0.1)
+    keyboard.release(key)
+
+# Reasons:
+# A. It has no side effects and allows other functions to use it with the variable "key".
+# B. It treats functions as first-class citizens by encapsulating key press and release actions.
+# Also, this is better; 1, It looks cleaner and more concise and 2, It's interchangable
+"""
